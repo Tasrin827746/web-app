f9198b8e9514a11747a6c1a1671b2b54af8fdce2
Morsala Tasrin <morsalatasrin@gmail.com>
2025-04-10 09:21:22 +0600

    ....

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const bundleAnalyzer = withBundleAnalyzer({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
+    ignoreDuringBuilds: true,
     dirs: ["."],
   },
   poweredByHeader: false,
